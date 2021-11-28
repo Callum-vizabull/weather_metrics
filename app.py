fea228e42afc21d84e0aaa8c79f0700b866fe2e9
callum-vizabull <ubuntu@ip-172-31-91-27.ec2.internal>
2021-11-28 04:19:40 +0000

    bug fixed duplicate deletion

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from chalice import Chalice
 import datetime
 app = Chalice(app_name='weather_metrics')
 
-from chalicelib.test import test 
+from chalicelib import test 
 import chalicelib.test as create_dataframes
 @app.route('/')
 def index():
@@ -11,74 +11,34 @@ def index():
  
 import vbdb.db as db
 
-conn_string = 'postgres+psycopg2://postgres:vizavizavizaviza@postgres-prod-2020-05-11-06-52-cluster.cluster-ciuvybput2xn.us-east-1.rds.amazonaws.com/vizabase'
-_DB_CONN = db.DBConnection(url=conn_string)
 
-from chalicelib.dbcalls import (
-    get_engine,
-)
 
-def write_weather_by_period(period,lookback=180):
-    df = test(period,lookback)
-    date_string =  str(datetime.date.today())
-    df["update_day"] = date_string
-    df["period"] = period
-    df.to_sql("weather_metric_table",conn_string,if_exists='append',index=False)
-    try:    
-        delete_query = """
-        DELETE FROM weather_metric_table
-            WHERE update_day NOT IN
-            (
-                SELECT MAX(update_day) AS max_date
-                FROM weather_metric_table
-                GROUP BY ticker,region,period,date
-            );"""
-        with get_engine().connect() as con:
-            con.execute(delete_query)
-    except Exception as e:
-        print(e)
-        print(period,"deletion")
-
-def write_weather_yoy(period):
-    df = create_dataframes.get_yoy_change(period)
-    date_string =  str(datetime.date.today())
-    df["update_day"] = date_string
-    df["period"] = period
-    return df
-    df.to_sql("weather_yoy_metric_table",conn_string,if_exists='append',index=False)
-    try:    
-        delete_query = """
-        DELETE FROM weather_yoy_metric_table
-            WHERE update_day NOT IN
-            (
-                SELECT MAX(update_day) AS max_date
-                FROM weather_yoy_metric_table
-                GROUP BY ticker,region,period,date
-            );"""
-        with get_engine().connect() as con:
-            con.execute(delete_query)
-    except Exception as e:
-        print(e)
-        print(period,"deletion")
         
-schedule_string ='cron(03 00 ? * * *)'
+schedule_string ='cron(21 01 ? * * *)'
+yoy_schedule_string ='cron(40 01 ? * * *)'
+backtesting_schedule_string  ='cron(10 02 ? * * *)'
 @app.schedule(schedule_string)
 def update_weather_week(event):
-        return write_weather_by_period("week")
+        return create_dataframes.write_weather_by_period("week")
 @app.schedule(schedule_string)
 def update_weather_month(event):
-        return write_weather_by_period("month")
+        return create_dataframes.write_weather_by_period("month")
 @app.schedule(schedule_string)
 def update_weather_year(event):
-        return write_weather_by_period("quarter")
-yoy_schedule_string ='cron(24 01 ? * * *)'
+        return create_dataframes.write_weather_by_period("quarter")
 @app.schedule(yoy_schedule_string)
 def update_weather_yoy(event):
-    write_weather_yoy("quarter")
-    write_weather_yoy("month")
-    return write_weather_yoy("week")
-    
+    create_dataframes.write_weather_yoy("quarter")
+    create_dataframes.write_weather_yoy("month")
+    create_dataframes.write_weather_yoy("week")
+    return "Done"
 @app.route('/weather_qtd/{ticker}/{start_date}/{end_date}')
 def weather_qtd(ticker,start_date,end_date):
     return create_dataframes.metrics_qtd_weather(ticker,start_date,end_date)
-
+import os
+@app.on_sqs_message(queue=os.environ['WEATHER_BACKTESTING_QUEUE'], batch_size=1)
+def update_backtesting(event):
+    return test.save_backtesting(event)
+@app.schedule(backtesting_schedule_string)
+def update_all_backtesting(event):
+    create_dataframes.run_backtesting()
--- chalicelib/test.py
+++ chalicelib/test.py
@@ -8,27 +8,36 @@ import json
 
 import chalicelib.agg as agg
 import chalicelib.datafun as datafun
+from chalicelib import metrics as metrics_lib
 from chalicelib.datafun import load_weather_daily_dataframe, generate_quarterly_excel,generate_monthly_excel
 
 import vbdb.db as db
 
 from chalicelib import util
 
-conn_string = 'postgres+psycopg2://postgres:vizavizavizaviza@postgres-prod-2020-05-11-06-52-cluster.cluster-ciuvybput2xn.us-east-1.rds.amazonaws.com/vizabase'
-_DB_CONN = db.DBConnection(url=conn_string)
+import re
+import chalicelib
 
-companies = list(_DB_CONN.session.query(db.Company))
+from chalicelib import dbcalls
+import math
+import boto3
+from chalicelib.dbcalls import (
+    get_engine,
+)
+
+
+companies = list(util.get_db_conn().session.query(db.Company))
 
 def test(period,lookback=180):
     start_date = datetime.date.today() - datetime.timedelta(days=lookback)
-    df = load_weather_daily_dataframe(start_date,connection = conn_string)
+    df = load_weather_daily_dataframe(start_date,connection = util.get_db_conn_string())
 
     df_list = []
     for i in range(4):
         companies_in_qtr = [c for c in companies if c.weather and len(c.locations)>0 and c.fiscal_yr_end_month % 4 == i
                            and sum([x.store_count for x in c.locations]) > 0]
 
-        date= datetime.date(2021,i+1,1)
+        date= datetime.date(2021,i+3,1)
         if period=="week":
             period_string="custom_weekly"
         if period == "month":
@@ -48,11 +57,10 @@ def test(period,lookback=180):
             temp_df_list.append(df_temp)
         large_df_list.append(pd.concat(temp_df_list,axis=0).pivot_table(values="value",columns="column_name",index=["ticker","region","date"]).reset_index())
     test_df = pd.concat(large_df_list,axis=0)
-    oldest_date = test_df.date.min()
-    return test_df[test_df.date!=oldest_date]
+    return test_df[test_df.date> pd.to_datetime(start_date)]
     
 def get_yoy_change(period):
-    period_df = pd.read_sql(f"Select * from weather_metric_table where period ='{period}';",con=conn_string)
+    period_df = pd.read_sql(f"Select * from weather_metric_table where period ='{period}';",con=util.get_db_conn_string())
     metrics = [c for c in period_df.columns if c not in ["ticker","region","date","update_day","period"]]
     if period =="quarter" or period=="month":
         period_df["period_of_year"] = period_df.date.apply(lambda x : (x.month))
@@ -78,3 +86,175 @@ def metrics_qtd_weather(ticker,start,end):
     query = f"Select * from weather_yoy_metric_table where  ticker='{ticker}' and date < '{str(end)}' and date > '{str(start)}';"
     df_result = pd.read_sql(query, con=util.get_db_conn_string())
     return json.dumps(list(df_result.to_dict('records')), default=util.alchemyencoder)
+
+
+def earning_date_to_quarter_start(date,fiscal_yr_end_month):
+    in_month = (int(date.strftime('%m')) - fiscal_yr_end_month  -1)% 12
+    in_quarter = math.floor((in_month ) / 3)
+    target_quarter = (in_quarter -1)  % 4
+    target_month = (target_quarter) * 3
+    target_year = date.year if target_month < in_month else date.year-1
+    # print(in_month,in_quarter,target_quarter)
+    return datetime.date(target_year,(target_month + fiscal_yr_end_month - 1) % 12 + 1,1)
+def get_revenue_df(ticker,debug=False):
+    try:
+        revenue_df = pd.read_sql(f"""Select * from revenue_table where "Page_URL" like '%%{ticker}%%';""", util.get_db_conn_string())
+        revenue_df['Ticker'] = revenue_df.Page_URL.apply(lambda x: re.search("(q=)(.*)", x).group(2))
+        revenue_df = revenue_df[~revenue_df.Ticker.isna()]
+        revenue_df.Ticker = revenue_df.Ticker.apply(lambda x: x.replace("(", "").replace(")", ""))
+        revenue_df = revenue_df.drop('Unnamed: 0', axis=1)
+        if debug:
+            return revenue_df
+    except:
+        print("problem getting revenue df for ",ticker)
+        raise RuntimeError(ticker)
+
+    try:
+        big_list = []
+        i = 0
+        for x in revenue_df[revenue_df.Ticker == ticker].iloc[0][9:-1].values:
+            if i % 8 == 0:
+                big_list.append([])
+            big_list[-1].append(x)
+            i += 1
+        revenues = pd.DataFrame(big_list, columns=list(map(lambda x: x.replace("1", ""), revenue_df.iloc[0][9:17].index))).drop("Guide", axis=1)
+    
+        revenues.Date = revenues.Date.apply(util.to_date)
+        revenues.ticker = ticker
+        revenues = revenues.sort_values("Date")
+        for c in revenues.columns[2:]:
+            revenues[c] = revenues[c].apply(util.dollar_to_float)
+        revenues = revenues.sort_values("Date")
+        revenues['EXP_GROWTH'] = revenues.Cons_Rev / revenues.shift(1).Rev
+        revenues['BEAT'] = revenues.Rev > revenues.Cons_Rev
+        revenues['GROWTH'] = revenues.Rev / revenues.shift(1).Rev
+    except:
+        print("problems with processing revenues for ",ticker)
+        raise RuntimeError(ticker)
+    return revenues    
+
+def save_backtesting_on_ticker(ticker,date_string,debug=False):
+    try:
+        signal_df = pd.read_sql(f"""Select * from weather_yoy_metric_table where ticker='{ticker}' and 
+            period = 'quarter' and region ='Total';""",con=util.get_db_conn_string())
+        signal_df = signal_df.sort_values("date",ascending=False).drop_duplicates(["date"])
+        
+        fiscal_year_end =dbcalls.get_db_conn().session.query(db.Company).filter(db.Company.id ==dbcalls.get_ticker_id()[ticker]).all()[0].fiscal_yr_end
+        metrics = ['cold_count', 'heat_count',
+               'rain_and_sleet', 'rain_count', 'sleet', 'sleet_count', 'snow',
+               'snow_count', 'temp_24h_mean', 'weekend_cold_count',
+               'weekend_heat_count', 'weekend_rain_count', 'weekend_sleet_count',
+               'weekend_snow_count']
+        signal_df.drop(["ticker","region","update_day","period"],axis=1)
+        values_df = signal_df[metrics+["date"]].iloc[[0]]
+        input_df = signal_df[metrics+["date"]].copy()
+        for m in metrics:
+            input_df[m] = input_df[m]>0
+    
+    
+        revenue_ticker_df = get_revenue_df(ticker)[["Date","Qtr","BEAT"]]
+        revenue_ticker_df["quarter_start"]=revenue_ticker_df.Date.apply(lambda x: pd.to_datetime(
+            earning_date_to_quarter_start(x,fiscal_year_end.month)))
+        scored_df = pd.merge(revenue_ticker_df,input_df,left_on="quarter_start",right_on="date").sort_values("date").drop(["Date","quarter_start"],axis=1)
+        signal, prediction, strength,meta_df  = metrics_lib.new_get_best_signal(scored_df.iloc[1:],scored_df.iloc[[0]])
+        results_dict = {}
+    
+        results_dict["final_value"] = prediction.values[0]
+        results_dict["final_wr"] = strength.values[0]
+        for k,v in meta_df.items():
+            results_dict[k]=v
+        results_dict["update_date"] = date_string
+        results_dict["ticker"] = ticker
+        results_dict["final_wr"]=strength[0]
+        results_dict["final_value"]=   prediction[0]
+        
+        wr_df = scored_df.copy()
+        for c in wr_df.columns:
+            if c!="BEAT":
+                wr_df[c+"_wr"] = wr_df[c]==wr_df.BEAT
+                
+        results_dict.update(wr_df[[c for c in wr_df.columns if c!="BEAT" and c!="Qtr" and c!="date"]].iloc[1:].mean().to_dict())
+        results_dict.update(signal_df[metrics].iloc[0].to_dict())
+        if debug:
+            return results_dict 
+        else:
+            pd.DataFrame(results_dict, index=[0]).to_sql("saved_weather_backtesting", con=dbcalls.get_db_conn_string(), index=False, if_exists="append")
+            delete_query = """ DELETE FROM saved_weather_backtesting where update_date not in (select max(update_date) from saved_weather_backtesting) and ticker = '{ticker}';"""
+            with dbcalls.get_engine().connect() as con:
+                con.execute(delete_query)
+    except:
+        print("failed on ",ticker,date_string)
+def save_backtesting(event):
+    date_string =  str(datetime.date.today())
+    for record in event:
+        body = json.loads(record.body)
+        for item in body:
+            ticker = item["ticker"]
+            save_backtesting_on_ticker(ticker,date_string)
+    return {"outcome":"success"}
+
+def run_backtesting():
+    sqs_client = boto3.client('sqs')
+    sqs_queue_url_ticker = sqs_client.get_queue_url(QueueName="weather-metrics-backtesting")['QueueUrl']
+                                          
+    query = "Select distinct(ticker) from weather_yoy_metric_table"
+    tickers = [x[0] for x in pd.read_sql(query,con=util.get_db_conn_string()).values]
+    for ticker in tickers:
+        msg_body = [{"ticker":ticker}]
+        # print(msg_body)
+        sqs_client.send_message(QueueUrl=sqs_queue_url_ticker,
+                                              MessageBody=json.dumps(msg_body, cls=util.DecimalEncoder))
+def write_weather_by_period(period,lookback=180):
+    df = test(period,lookback)
+    date_string =  str(datetime.date.today())
+    df["update_day"] = date_string
+    df["period"] = period
+    df.to_sql("weather_metric_table",util.get_db_conn_string(),if_exists='append',index=False)
+    try:    
+        delete_query = """ 
+        DELETE FROM weather_metric_table wm
+        USING (
+                SELECT *,row_number() OVER (PARTITION BY period,date,ticker,region ORDER BY update_day DESC)  as rn 
+                FROM weather_metric_table
+        ) del
+        WHERE del.period = wm.period
+        AND del.region = wm.region
+        AND del.date = wm.date
+        AND del.update_day = wm.update_day
+        and del.ticker = wm.ticker
+        AND del.rn >1
+        ;"""
+        with get_engine().connect() as con:
+            con.execute(delete_query)
+    except Exception as e:
+        print(e)
+        print(period,"deletion")
+
+def write_weather_yoy(period,debug=False):
+    df = get_yoy_change(period)
+    date_string =  str(datetime.date.today())
+    df["update_day"] = date_string
+    df["period"] = period
+    if debug:
+        return df
+    else:
+        df.to_sql("weather_yoy_metric_table",util.get_db_conn_string(),if_exists='append',index=False)
+        try:    
+            delete_query = """ 
+            DELETE FROM weather_yoy_metric_table wm
+            USING (
+                    SELECT *,row_number() OVER (PARTITION BY period,ticker,region,date ORDER BY update_day DESC)  as rn 
+                    FROM weather_yoy_metric_table
+            ) del
+            WHERE del.period = wm.period
+            AND del.region = wm.region
+            AND del.date = wm.date
+            AND del.update_day = wm.update_day
+            and del.ticker = wm.ticker
+            AND del.rn >1
+            ;"""
+            with get_engine().connect() as con:
+                con.execute(delete_query)
+        except Exception as e:
+            print(e)
+            print(period,"deletion")
